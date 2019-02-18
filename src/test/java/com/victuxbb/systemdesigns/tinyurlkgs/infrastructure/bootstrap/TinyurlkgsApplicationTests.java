e35bbf379c2e572488819911561d249cf842a8db
Arkioner <ioner468@gmail.com>
2019-02-18 16:49:10 +0100

    fix test structure

--- src/test/java/com/victuxbb/systemdesigns/tinyurlkgs/infrastructure/bootstrap/TinyurlkgsApplicationTests.java
+++ src/test/java/com/victuxbb/systemdesigns/tinyurlkgs/infrastructure/bootstrap/TinyurlkgsApplicationTests.java
@@ -1,11 +1,13 @@
-package com.victuxbb.systemdesigns.tinyurlkgs;
+package com.victuxbb.systemdesigns.tinyurlkgs.infrastructure.bootstrap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class TinyurlkgsApplicationTests {
 
