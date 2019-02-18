2dd319581273de92006e321bb6dcfb39416be3ad
Arkioner <ioner468@gmail.com>
2019-02-18 16:46:12 +0100

    spring initializr

---
+++ src/main/java/com/victuxbb/systemdesigns/tinyurlkgs/infrastructure/bootstrap/TinyurlkgsApplication.java
@@ -0,0 +1,13 @@
+package com.victuxbb.systemdesigns.tinyurlkgs.infrastructure.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlkgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyurlkgsApplication.class, args);
+	}
+
+}
